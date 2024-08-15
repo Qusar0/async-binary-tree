4990c70bd52d425135d1f72cff9b27e9f9a4bd3f
Ковалев Артем Сергеевич <as.kovalev@nicetu.spb.ru>
2024-08-15 11:07:12 +0300

    Update test_binary_tree_async_iterator.py

--- tests/test_binary_tree_async_iterator.py
+++ tests/test_binary_tree_async_iterator.py
@@ -1,14 +1,36 @@
 import unittest
-from binary_tree_async_iterator.async_postorder_binary_tree_iterator import (
-    AsyncIteratorWithoutGenerator,
-    AsyncIteratorWithGenerator,
-    TreeNode,
-)
+from binary_tree_async_iterator.async_iterator_with_generator import AsyncIteratorWithGenerator
+from binary_tree_async_iterator.async_iterator_without_generator import AsyncIteratorWithoutGenerator
+from binary_tree_async_iterator.tree_node import TreeNode
 
 
-class MyTestCase(unittest.IsolatedAsyncioTestCase):
-    async def test_something(self):
-        self.assertEqual(True, True)
+class TestAsyncIteratorWithoutGenerator(unittest.IsolatedAsyncioTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.root = TreeNode(1)
+        cls.root.right = TreeNode(2)
+        cls.root.left = TreeNode(3)
+        cls.root.left.right = TreeNode(4)
+        cls.root.left.left = TreeNode(5)
+
+
+    async def test_post_order(self):
+        expected_result = [5, 4, 3, 2, 1]
+        result = [item async for item in AsyncIteratorWithoutGenerator(self.root)]
+
+        self.assertEqual(expected_result, result)
+
+    async def test_anext(self):
+        iterator = AsyncIteratorWithoutGenerator(self.root)
+
+        with self.assertRaises(StopAsyncIteration):
+            while True:
+                await anext(iterator)
+
+    async def test_async_for(self):
+        async for _ in AsyncIteratorWithoutGenerator(self.root):
+            pass
 
 
 if __name__ == '__main__':
