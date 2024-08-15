3966d381658acf21945e5e4ba0459f5a0e2a711f
Ковалев Артем Сергеевич <as.kovalev@nicetu.spb.ru>
2024-08-15 10:38:36 +0300

    Update async_postorder_binary_tree_iterator.py

--- binary_tree_async_iterator/async_postorder_binary_tree_iterator.py
+++ binary_tree_async_iterator/async_postorder_binary_tree_iterator.py
@@ -1,46 +1,61 @@
 import asyncio
+from async_iterator_with_generator import AsyncIteratorWithGenerator
+from async_iterator_without_generator import AsyncIteratorWithoutGenerator
+from tree_node import TreeNode
+from random import random
 
 
-class TreeNode:
-    """Узел бинарного дерева."""
-
-    def __init__(self, val=0, left=None, right=None, get_value_timeout_in_seconds=0):
-        """Инициализация."""
-        self.__val = val
-        self.get_value_timeout_in_seconds = get_value_timeout_in_seconds
-        self.left = left
-        self.right = right
-
-    async def get_value(self):
-        """Асинхронное получение значения узла дерева."""
-        if self.get_value_timeout_in_seconds is not None:
-            await asyncio.sleep(self.get_value_timeout_in_seconds)
-        return self.__val
+async def using_generator(root: TreeNode, generator: bool):
+    if generator:
+        async for item in AsyncIteratorWithGenerator(root):
+            print(item, end=" ")
+    else:
+        async for item in AsyncIteratorWithoutGenerator(root):
+            print(item, end=" ")
 
 
-async def postorder_generator(root: TreeNode):
-    """ Асинхронный генератор."""
+async def first_tree(with_generator: bool=False):
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.left.left = TreeNode(3)
+    root.left.left.right = TreeNode(4)
+    root.left.left.left = TreeNode(5, get_value_timeout_in_seconds=1)
+
+    await using_generator(root, with_generator)
 
 
-class AsyncIteratorWithGenerator:
-    """Реализация с использованием асинхронного генератора."""
+async def second_tree(with_generator: bool=False):
+    root = TreeNode(6)
+    root.right = TreeNode(7)
+    root.right.right = TreeNode(8, get_value_timeout_in_seconds=2)
+    root.left = TreeNode(9)
+    root.left.right = TreeNode(10)
 
-    def __init__(self, root: TreeNode):
-        """Инициализация."""
+    await using_generator(root, with_generator)
 
 
-class AsyncIteratorWithoutGenerator:
-    """Реализация без использования асинхронного генератора."""
+async def third_tree(with_generator: bool=False):
+    root = TreeNode(11)
+    root.right = TreeNode(12)
+    root.right.right = TreeNode(13, get_value_timeout_in_seconds=3)
+    root.right.right.right = TreeNode(14)
+    root.right.right.left = TreeNode(15)
 
-    def __init__(self, root: TreeNode):
-        """Инициализация."""
+    await using_generator(root, with_generator)
 
 
 async def main():
-    """ Демонстрируем одновременный проход по трем деревьям сначала с использованием класса AsyncIteratorWithGenerator,
-        затем проход по тем же деревьм с помощью AsyncIteratorWithoutGenerator.
-    """
+    print("Async iterator without generator")
+    await asyncio.gather(first_tree(),
+                         second_tree(),
+                         third_tree())
+    
+    print("\n\nAsync iterator with generator")
+    await asyncio.gather(first_tree(with_generator=True),
+                         second_tree(with_generator=True),
+                         third_tree(with_generator=True))
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+
